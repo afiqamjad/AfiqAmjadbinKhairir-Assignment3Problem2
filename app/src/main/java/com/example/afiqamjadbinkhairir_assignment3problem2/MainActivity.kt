153396fae262a34bf7baa7280803ae711e2e871b
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-02-14 23:28:22 -0500

    Finished button/edittext problem

--- app/src/main/java/com/example/afiqamjadbinkhairir_assignment3problem2/MainActivity.kt
+++ app/src/main/java/com/example/afiqamjadbinkhairir_assignment3problem2/MainActivity.kt
@@ -3,12 +3,24 @@ package com.example.afiqamjadbinkhairir_assignment3problem2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("MainActivity", "onCreate Was Called")
         setContentView(R.layout.activity_main)
+
+        val buttonHello = findViewById<Button>(R.id.helloButton)
+        val editText = findViewById<EditText>(R.id.editText)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        buttonHello.setOnClickListener {
+            editText.setText(R.string.helloText)
+            textView.setText(R.string.helloText)
+        }
     }
 
     override fun onStart() {
